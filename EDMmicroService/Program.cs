6353b0680391eb945615c2ebf0bc22d3197eecc9
Eric Faraut <eric.faraut@hotmail.fr>
2024-04-08 04:13:46 +0200

    API Program.cs file is updated.

--- EDMmicroService/Program.cs
+++ EDMmicroService/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,6 +13,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -22,4 +26,27 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+var fileTypes = new[]
+{
+    "PDF", "ODT", "ODS", "ODP", "ODG", "ODF"
+};
+
+app.MapGet("/findDocument", () =>
+{
+    var document = Enumerable.Range(1, 5).Select(index =>
+    new Document
+    (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        fileTypes[Random.Shared.Next(fileTypes.Length)]
+    ))
+    .ToArray();
+    return document;
+}).WithName("GetFileType")
+.WithOpenApi();
+
 app.Run();
+
+record Document(DateOnly DateCreated, string FileType)
+{
+
+}
