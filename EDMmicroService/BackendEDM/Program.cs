ab22cc77c5302b2816a8af14f3fb8d924d4a4a54
Eric Faraut <eric.faraut@hotmail.fr>
2024-04-09 15:29:31 +0200

    Code backup is done. Program runs as is.

--- EDMmicroService/BackendEDM/Program.cs
+++ EDMmicroService/BackendEDM/Program.cs
@@ -1,3 +1,5 @@
+using static Microsoft.AspNetCore.Http.Results;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -18,22 +20,82 @@ var fileTypes = new[]
     "PDF", "ODT", "ODS", "ODP", "ODG", "ODF"
 };
 
-app.MapGet("/findDocument", () =>
+#region data
+var files = new List<File>();
+#endregion
+
+/// <summary> HTTP verbs </summary>
+#region POST
+app.MapPost("/files", (File file) =>
 {
-    var document = Enumerable.Range(1, 5).Select(index =>
-    new Document
+    files.Add(file);
+    return TypedResults.Created("/files/{id}", file);
+});
+#endregion
+
+#region GET
+/// <summary> Get 
+/// all, 
+/// random typed,
+/// all files and 
+/// by id files.
+/// 
+/// ToDo:
+/// app.MapGet("/files/{id}", Results<Ok<File>, NotFound>(int id) =>
+///{
+///    var file = files.SingleOrDefault(f => id == f.id);
+///    return file is null 
+///   ?
+///   TypedResults.NotFound()
+///    :
+///    TypedResults.Ok(file);
+///});
+/// </summary>
+app.MapGet("/", () =>
+{
+    var file = Enumerable.Range(0, 6).Select(index =>
+    new File
     (
+        index, 
+        string.Concat("Name", index),
         DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        fileTypes[Random.Shared.Next(fileTypes.Length)]
+        fileTypes[index],
+        false
     ))
     .ToArray();
-    return document;
-}).WithName("GetFileType")
+    return file;
+}).WithName("GetAllFileTypes")
 .WithOpenApi();
+
+app.MapGet("/randomFileTypes", () =>
+{
+    var file = Enumerable.Range(2, 4).Select(index =>
+    new File
+    (
+        index,
+        string.Concat("Name", index),
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        fileTypes[Random.Shared.Next(fileTypes.Length)],
+        false
+    ))
+    .ToArray();
+    return file;
+}).WithName("GetRandomFileTypes")
+.WithOpenApi();
+
+app.MapGet("/files", () => files);
+#endregion
+
+#region DELETE
+app.MapDelete("/files/{id}", (int id) =>
+{
+    files.RemoveAll(f => id == f.id);
+    return TypedResults.NoContent();
+});
+#endregion
 
 app.Run();
 
-record Document(DateOnly Date, string? FileType)
-{
-    
-}
+#region domain
+record File(int id, string Name, DateOnly Date, string? FileType, bool? isIndexed){}
+#endregion
