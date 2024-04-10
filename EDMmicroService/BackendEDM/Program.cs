27739d3c1fd879c4847b1549c62f841dd2e220ac
Eric Faraut <eric.faraut@hotmail.fr>
2024-04-10 15:43:54 +0200

    HTTP test file is updated and DI pattern is implemented. Code backup is done.

--- EDMmicroService/BackendEDM/Program.cs
+++ EDMmicroService/BackendEDM/Program.cs
@@ -1,9 +1,11 @@
 using static Microsoft.AspNetCore.Http.Results;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IFileService>(new InMemoryFileService());
 
 var app = builder.Build();
 
@@ -20,17 +22,40 @@ var fileTypes = new[]
     "PDF", "ODT", "ODS", "ODP", "ODG", "ODF"
 };
 
+app.UseRewriter(new RewriteOptions().AddRedirect("documents/(.*)", "files/$1"));
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow} Started API]");
+    await next(context);
+    Console.WriteLine($"[{context.Request.Method} {context.Request.Path} {DateTime.UtcNow}] Ended API");
+});
+
 #region data
-var files = new List<File>();
+var files = new List<File>(){};
 #endregion
 
 /// <summary> HTTP verbs </summary>
 #region POST
-app.MapPost("/files", (File file) =>
+app.MapPost("/files", (File file, IFileService service) =>
 {
-    files.Add(file);
+    service.AddFile(file);
     return TypedResults.Created("/files/{id}", file);
-});
+})
+.AddEndpointFilter(async (context, next) =>
+{
+    var fileArgument = context.GetArgument<File>(0);
+    var errors = new Dictionary<string, string[]>();
+    if(fileArgument.DueDate.ToDateTime(new TimeOnly()) < DateTime.UtcNow)
+    {
+        errors.Add(nameof(File.DueDate), new string[]{"Cannot have a due date in the past"});
+    }
+    if(errors.Count > 0)
+        return Results.ValidationProblem(errors);
+
+    return await next(context);
+})
+;
 #endregion
 
 #region GET
@@ -41,9 +66,9 @@ app.MapPost("/files", (File file) =>
 /// by id files.
 /// 
 /// ToDo:
-/// app.MapGet("/files/{id}", Results<Ok<File>, NotFound>(int id) =>
+/// app.MapGet("/files/{id}", Results<Ok<File>, NotFound>(int id, IFileService service) =>
 ///{
-///    var file = files.SingleOrDefault(f => id == f.id);
+///    var file = service.GetFileById(id);
 ///    return file is null 
 ///   ?
 ///   TypedResults.NotFound()
@@ -83,13 +108,13 @@ app.MapGet("/randomFileTypes", () =>
 }).WithName("GetRandomFileTypes")
 .WithOpenApi();
 
-app.MapGet("/files", () => files);
+app.MapGet("/files", (IFileService service) => service.GetFiles());
 #endregion
 
 #region DELETE
-app.MapDelete("/files/{id}", (int id) =>
+app.MapDelete("/files/{id}", (int id, IFileService service) =>
 {
-    files.RemoveAll(f => id == f.id);
+    service.DeleteFileById(id);
     return TypedResults.NoContent();
 });
 #endregion
@@ -97,5 +122,42 @@ app.MapDelete("/files/{id}", (int id) =>
 app.Run();
 
 #region domain
-record File(int id, string Name, DateOnly Date, string? FileType, bool? isIndexed){}
+record File(int Id, string Name, DateOnly DueDate, string? FileType, bool? IsIndexed){}
+
+interface IFileService
+{
+    File? GetFileById(int id);
+    List<File> GetFiles();
+    void DeleteFileById(int id);
+    File AddFile(File file);
+}
+
+/// <summary>
+/// Decoupled to replace it by a database handler.
+/// </summary>
+class InMemoryFileService : IFileService
+{
+    private readonly List<File> _files = [];
+
+    public File AddFile(File file)
+    {
+        _files.Add(file);
+        return file;
+    }
+
+    public void DeleteFileById(int id)
+    {
+        _files.RemoveAll(file => id == file.Id);
+    }
+
+    public File GetFileById(int id)
+    {
+        return _files.SingleOrDefault(file => id == file.Id);
+    }
+
+    public List<File> GetFiles()
+    {
+        return _files;
+    }
+}
 #endregion
