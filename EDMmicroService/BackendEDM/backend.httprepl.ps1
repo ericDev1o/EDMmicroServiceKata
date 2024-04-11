503ea80c8fe9eec521af8c79bb5cd6189331e7ec
Eric Faraut <eric.faraut@hotmail.fr>
2024-04-12 00:39:11 +0200

    Code backup before testing is done.

---
+++ EDMmicroService/BackendEDM/backend.httprepl.ps1
@@ -0,0 +1,6 @@
+# c.f. https://learn.microsoft.com/en-us/aspnet/core/web-api/http-repl/?view=aspnetcore-8.0&tabs=windows
+
+dotnet tool install -g Microsoft.dotnet-httprepl
+
+httprepl http://localhost:5173
+
